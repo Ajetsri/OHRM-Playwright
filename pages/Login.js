111947b419685cd37cce48db5b10d12eeef9a763
Ajetsri <sri291399@gmail.com>
2025-10-03 23:46:58 +0530

    logout

--- pages/Login.js
+++ pages/Login.js
@@ -1,11 +1,16 @@
+import { expect } from "@playwright/test";
+
 export class Login {
   constructor(page) {
     this.page = page;
+    this.LoginHead='[class="oxd-text oxd-text--h5 orangehrm-login-title"]'
     this.usernameInput = 'input[name="username"]';
     this.passwordInput = 'input[name="password"]';
     this.loginButton = 'button[type="submit"]';
     this.errorMessage = '.oxd-alert-content-text';
     this.dashboardHeader = 'h6.oxd-topbar-header-breadcrumb-module';
+    this.UserpassworderrorMsg='//*[@class="oxd-text oxd-text--span oxd-input-field-error-message oxd-input-group__message"]';
+    this.lastnamedropdown='[class="oxd-dropdown-menu"]'
   }
 
   async goto() {
@@ -16,5 +21,18 @@ export class Login {
     await this.page.fill(this.usernameInput, username);
     await this.page.fill(this.passwordInput, password);
     await this.page.click(this.loginButton);
+  }
+  async Logout(){
+    await this.page.lastnamedropdown.select_option("label=4");
+    await expect(page.LoginHead).tobeVisible();
+  }
+  async ErrorMessageRequired()
+  {
+    await this.page.click(this.loginButton);
+    for(i=0;i<2;i++)
+      {
+        await expect(page.UserpassworderrorMsg.nth(i)).tobeVisible();
+        await expect(page.UserpassworderrorMsg.nth(i)).toContainText('Required');
+      }
   }
 }
--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import { Login } from '../pages/Login.js';
 import loginData from '../testdata/loginData.json' assert { type: "json" };
 
-test.describe('Login Tests', () => {
+test.describe('Login Tests', () => {                      //This is grouping tests, It loops
 
   test('Valid Login Test', async ({ page }) => {
     const loginPage = new Login(page);
@@ -19,5 +19,18 @@ test.describe('Login Tests', () => {
       await expect(page.locator(loginPage.errorMessage)).toBeVisible();
     });
   }
+
+});
+test('No input Login -Display Required Field Error message',async({page})=>{
+  const loginPage=new Login(page);
+  await loginPage.goto();
+  await loginPage.ErrorMessageRequired();
+})
+test('Logout',async({page})=>{
+  const loginPage=new Login(page);
+  await loginPage.goto();
+  await loginPage.login(loginData.valid.username, loginData.valid.password);
+  await expect(page.locator(loginPage.dashboardHeader)).toContainText('Dashboard');
+  
 
 });
